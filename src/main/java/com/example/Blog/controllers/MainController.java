02e084547d16e2e89a868598f44431ccd4cf89d7
ye-seob <jakalob103@naver.com>
2024-07-23 01:21:38 +0900

    first commit

---
+++ src/main/java/com/example/Blog/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.Blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("userName","예섭");
+
+        return "mainPage";
+    }
+}
